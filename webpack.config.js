d56f0e05ba0ccff12d07b5b9243b7c356088efa4
Thiago Lopes <aslthiago@gmail.com>
2022-09-12 11:47:39 -0300

    js: pre-processing of files and libraries

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
 
 const config  = {
     mode: 'development',
@@ -23,8 +24,30 @@ const config  = {
         compress: true,
         historyApiFallback: true,
     },
-}
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+};
 
 module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+    } else {
+        config.mode = 'development';
+    }
     return config;
 };
