8e29ea9dd28723eb70d780f8a0b5ed58d2f8fb3e
Thiago Lopes <aslthiago@gmail.com>
2022-09-12 17:55:04 -0300

    css: adding fontawesome and material icons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import './style.css'
+import './style.css';
 
-console.log('Hello, world!')
+import '@material-tailwind/html/scripts/alert';
+import '@material-tailwind/html/scripts/collapse';
+import '@material-tailwind/html/scripts/colored-shadow';
+import '@material-tailwind/html/scripts/dialog';
+import '@material-tailwind/html/scripts/input';
+import '@material-tailwind/html/scripts/menu';
+import '@material-tailwind/html/scripts/popover';
+import '@material-tailwind/html/scripts/progress';
+import '@material-tailwind/html/scripts/ripple';
+import '@material-tailwind/html/scripts/select';
+import '@material-tailwind/html/scripts/tabs';
+import '@material-tailwind/html/scripts/toggle';
+import '@material-tailwind/html/scripts/tooltip';
+
+import '@fortawesome/fontawesome-free/js/all';
+
+console.log('Hello, world!');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const withMT = require("@material-tailwind/html/utils/withMT")
+
+module.exports = withMT({
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+});
